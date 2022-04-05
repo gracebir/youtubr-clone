46b9b51cf0a8d1f09b9c5747041ca6699fbf85ec
gracebir <grace.devolop@gmail.com>
2022-04-05 19:41:56 +0200

    install packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GlobalStyle } from '../styles/globalStyle'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle/>
+     <Component {...pageProps} />
+    </>
+    )
+   
+
 }
 
 export default MyApp
