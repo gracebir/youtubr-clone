c1613a86495e139dd0d3515404fa22e4dde77485
gracebir <grace.devolop@gmail.com>
2022-04-07 20:34:30 +0200

    merge sidebar, video, and navbar to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from 'next';
-import { FaBeer } from 'react-icons/fa';
+
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>youtube clone <FaBeer/></h1>
+      
     </div>
   )
 }
